b8948c4e2fd0f7942429a38d9e22fbd061c769be
drew1718 <30819197+drew1718@users.noreply.github.com>
2019-08-23 14:17:14 +1000

    Initial commit

---
+++ Houseplanto/Houseplanto/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Houseplanto.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Houseplanto/Houseplanto/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Houseplanto.Models;
+
+namespace Houseplanto.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
